0669692e6105b1bad81305391d3762a4820cc15f
Syed Murtuza Ali <134627651+syed-murtuzaali@users.noreply.github.com>
2025-07-04 12:13:46 -0700

    Add files via upload

---
+++ alerts/app.js
@@ -0,0 +1,16 @@
+// 1. Declare a variable called username. 
+
+// let username;
+
+
+// 2. Declare a variable called myName & assign to it a string 
+// that represents your Full Name. 
+
+// var myName = "Syed Murtuza Ali"
+
+// alert(myName);
+
+// 3. Write script to 
+// a) Declare a JS variable, titled message. 
+// b) Assign “Hello World” to variable message 
+// c) Display the message in alert box. 
